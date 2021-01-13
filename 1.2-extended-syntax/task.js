fad7d32dd3125c05132e3fd3b6eb74050cdd7c5e
Stanislav Pronkin <stanley_@mail.ru>
2021-01-13 23:57:07 +0700

    task 1 done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,32 @@
-function getResult(a,b,c){
+function getResult(a, b, c) {
     // код для задачи №1 писать здесь
-    // return x;
+    'use strict';
+    let D; //Дискриминант
+    D = Math.pow(b, 2) - 4 * a * c;
+
+    let x = [];
+    let value;
+
+    if (D > 0) {
+        value = (-b + Math.sqrt(D)) / 2 * a;
+        x.push(value);
+        value = (-b - Math.sqrt(D)) / 2 * a;
+        x.push(value);
+    } else if (D === 0) {
+        value = (-b + Math.sqrt(D)) / 2 * a;
+        x.push(value);
+    }
+    
+
+    return x;
 }
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
+function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
     // return result;
 }
