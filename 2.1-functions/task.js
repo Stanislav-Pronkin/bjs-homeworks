7998dd636b7864faf191697085b02d9c46f993c7
Stanislav Pronkin <stanley_@mail.ru>
2021-01-26 13:29:54 +0700

    task 2 done

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -11,7 +11,7 @@ function getSolutions(a, b, c) {
     } else if (D > 0) {
         let x1 = (-b + Math.sqrt(D)) / 2 * a;
         let x2 = (-b - Math.sqrt(D)) / 2 * a;
-        return {D: D, roots: [x1, x2] };
+        return { D: D, roots: [x1, x2] };
     }
 }
 
@@ -31,3 +31,47 @@ function showSolutionMessage(a, b, c) {
 
 //Задача №2
 
+let marksAvg;
+let marks;
+let grades = [];
+
+function getAverageScore(data) {
+    let dataAvg = {};
+    let gradesAvgSum = 0;
+    let itemSum = 0;
+
+    for (let prop in data) {
+        itemSum++;
+        marks = data[prop];
+
+        getAverageMark(marks);
+        dataAvg[prop] = marksAvg;
+        gradesAvgSum += marksAvg;
+    }
+
+    //console.log(gradesAvgSum);
+    //console.log(itemSum);
+    if (Object.keys(data).length === 0) {
+        dataAvg.average = 0;
+    } else {
+        dataAvg.average = gradesAvgSum / itemSum;
+    }
+
+    //console.log(grades);
+    //console.log(dataAvg);
+    return dataAvg;
+}
+
+function getAverageMark(marks) {
+    if (marks.length == 0) {
+        marksAvg = 0;
+    } else {
+        let sum = 0;
+        for (i = 0; i < marks.length; i++) {
+            sum += marks[i];
+            grades.push(marks[i]);
+        }
+        marksAvg = sum / marks.length;
+    }
+    return marksAvg;
+}
