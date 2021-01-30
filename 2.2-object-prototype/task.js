8804aa9231aed9bdc52ae8c0053fbb7611167b4b
Stanislav Pronkin <stanley_@mail.ru>
2021-01-31 00:53:36 +0700

    homeworks done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,34 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let str = this.toLowerCase().split(' ').join('');
+    let text = str.split('');
+
+    return text.join('') === text.reverse().join('');
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+    let count = 0;
+
+    marks.forEach(function(item) {
+        sum += item;
+        ++count;
+    })
+    
+    average = sum / count;
+    roundedAverage = (sum === 0) ? 0 : Math.round(average);
+    
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date();
+    let date = new Date(birthday);
+    birthday = +date;
+    let diff = now - birthday;
+    age = diff / 86400000 / 365.25;
+    verdict = (age >= 18) ? true : false;
+    return verdict
 }
